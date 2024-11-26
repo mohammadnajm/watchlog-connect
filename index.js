40c927d3bd60046a3cc9e54d9236159e547ecce5
mohammad <mohammadnajm75@gmail.com>
2024-11-26 15:05:50 +0330

    fix bug

--- index.js
+++ index.js
@@ -1,132 +1,110 @@
-const WebSocket = require('ws');
+const fetch = require('node-fetch');
+const axios = require('axios');
 
-const serverURL = 'ws://127.0.0.1:3775/'; // Replace with your server URL
-let ws = null; // WebSocket instance
-let reconnectInterval = 1000; // Initial reconnect interval
-let reconnectTimeoutId = null; // Reconnect timeout ID
+const serverURL = 'http://127.0.0.1:3774/'; // Replace with your server URL
 
 class SocketCli {
-    constructor() {
-        this.connect();
-    }
 
-    connect() {
-        ws = new WebSocket(serverURL);
-
-        ws.on('open', () => {
-            reconnectInterval = 1000;
-            reconnectTimeoutId = null;
-
-        });
-
-        ws.on('close', () => {
-            this.reconnect();
-        });
-
-        ws.on('error', (error) => {
-            this.reconnect();
-        });
-    }
 
     increment(metric = null, count = 1) {
         if (typeof metric === 'string' && typeof count === 'number' && count > 0) {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'increment', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'increment'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+
+            } catch (error) { }
+
         }
     }
     decrement(metric = null, count = 1) {
         if (typeof metric === 'string' && typeof count === 'number' && count > 0) {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'decrement', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'decrement'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+            } catch (error) { }
+
+
         }
     }
     distribution(metric = null, count) {
         if (typeof metric === 'string' && typeof count === 'number') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'distribution', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'distribution'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+            } catch (error) { }
+
         }
     }
     gauge(metric = null, count) {
         if (typeof metric === 'string' && typeof count === 'number') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'gauge', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'gauge'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+
+            } catch (error) { }
+
         }
     }
     percentage(metric = null, count) {
         if (typeof metric === 'string' && typeof count === 'number' && count > 0 && count <= 100) {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'percentage', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'percentage'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+            } catch (error) { }
+
         }
     }
     systembyte(metric = null, count) {
         if (typeof metric === 'string' && typeof count === 'number' && count > 0) {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'systembyte', metric, count }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
+            try {
+                axios({
+                    url: serverURL + `?method=${'systembyte'}&metric=${metric}&count=${count}`
+                }).then().catch(err => null)
+            } catch (error) { }
+
         }
     }
-    successLog(service = null, message) {
-        if (typeof service === 'string' && typeof message === 'string') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'log', service, message, status:1 }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
-        }
-    }
-    errorLog(service = null, message) {
-        if (typeof service === 'string' && typeof message === 'string') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'log', service, message, status:-1 }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
-        }
-    }
-    warningLog(service = null, message) {
-        if (typeof service === 'string' && typeof message === 'string') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'log', service, message, status:2 }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
-        }
-    }
-    log(service = null, message) {
-        if (typeof service === 'string' && typeof message === 'string') {
-            if (ws && ws.readyState === WebSocket.OPEN) {
-                ws.send(JSON.stringify({ method: 'log', service, message, status:0 }));
-            } else {
-                // console.log("Data cannot be sent to the watchlog agent")
-            }
-        }
-    }
-    reconnect() {
-        if (!reconnectTimeoutId && ws.readyState !== WebSocket.OPEN) {
-            console.log(`Reconnecting in ${reconnectInterval / 1000} seconds...`);
-            reconnectTimeoutId = setTimeout(() => {
-                reconnectTimeoutId = null;
-                this.connect();
-            }, reconnectInterval);
-        }
-    }
+
+    // successLog(service = null, message) {
+    //     if (typeof service === 'string' && typeof message === 'string') {
+    //         try {
+    //             fetch(serverURL, { method: "POST", body: JSON.stringify({ method: 'log', metric, count }) })
+    //         } catch (error) { }
+
+    //     }
+    // }
+    // errorLog(service = null, message) {
+    //     if (typeof service === 'string' && typeof message === 'string') {
+    //         if (ws && ws.readyState === WebSocket.OPEN) {
+    //             ws.send(JSON.stringify({ method: 'log', service, message, status: -1 }));
+    //         } else {
+    //             // console.log("Data cannot be sent to the watchlog agent")
+    //         }
+    //     }
+    // }
+    // warningLog(service = null, message) {
+    //     if (typeof service === 'string' && typeof message === 'string') {
+    //         if (ws && ws.readyState === WebSocket.OPEN) {
+    //             ws.send(JSON.stringify({ method: 'log', service, message, status: 2 }));
+    //         } else {
+    //             // console.log("Data cannot be sent to the watchlog agent")
+    //         }
+    //     }
+    // }
+    // log(service = null, message) {
+    //     if (typeof service === 'string' && typeof message === 'string') {
+    //         if (ws && ws.readyState === WebSocket.OPEN) {
+    //             ws.send(JSON.stringify({ method: 'log', service, message, status: 0 }));
+    //         } else {
+    //             // console.log("Data cannot be sent to the watchlog agent")
+    //         }
+    //     }
+    // }
 
 }
 
